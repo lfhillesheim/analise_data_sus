4b9d6973c2305da98e4c0e2244af2ef6da80e2ac
Lucas Ferreira Hillesheim <50627902+lfhillesheim@users.noreply.github.com>
2021-06-22 21:21:22 -0300

    Adicionado a função de calcular o IPCA

--- notebooks/funcoes/funcoes.py
+++ notebooks/funcoes/funcoes.py
@@ -1,16 +1,34 @@
 import pandas as pd
 from datetime import date
-import seaborn as sns
-import matplotlib.pyplot as plt
-from matplotlib import ticker
 
-def faz_alguma_coisa(coisa):
-    '''
-    Função que recebe uma alguma coisa e transforma em outra
-    Obs:retorna uma outra coisa
-    Parâmetros:
-    
-        coisa: alguma coluna do dataframe, tipo=pd.Series
-    '''
-    alguma_coisa = coisa
-    return alguma_coisa
+
+url_ipca = "https://raw.githubusercontent.com/lfhillesheim/analise_data_sus/main/data/ipca_94-2021.csv"
+ipca = pd.read_csv(url_ipca).set_index("ANO/MES")
+
+def correcao_monetaria_ipca(valor_passado, data_passada, data_correcao):
+  '''
+  -------------------------------------------------------------------------
+  Name: correcao monetaria
+  -------------------------------------------------------------------------
+  Função que busca no repositorio lfhillesheim/analise_data_sus/main/data/ipca_94-2021.csv os dados de IPCA e aplica correção monetária 
+
+  -------------------------------------------------------------------------
+  Parâmetros:
+
+  @param valor_passado: valor na data passa
+  @param data_passada : data passada  ano/mes
+  @param data_correcao: data correcao ano/mes
+
+
+  -------------------------------------------------------------------------
+  Exemplo
+
+  valor_pelo_ipca = correcao_monetaria(100, "2008/JAN", "2020/DEZ")
+
+  '''
+
+
+  i_passado = ipca.loc[data_passada][0]
+  i_atual = ipca.loc[data_correcao][0]
+
+  return  round(valor_passado * (i_atual/i_passado), 2)
