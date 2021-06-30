5855ad8b62a8273a03924271b117a1ff1f5a8158
Lucas Ferreira Hillesheim <50627902+lfhillesheim@users.noreply.github.com>
2021-06-30 20:26:44 -0300

    Update data_func_sus.py

--- notebooks/data_func_sus.py
+++ notebooks/data_func_sus.py
@@ -19,7 +19,7 @@ def correcao_monetaria_ipca(valor_passado, ipca, data_passada, data_correcao):
   Exemplo
   valor_pelo_ipca = correcao_monetaria(100, ipca, "2008/JAN", "2020/DEZ")
   '''
-
+  
 
   i_passado = ipca.loc[data_passada][0]
   i_atual = ipca.loc[data_correcao][0]
@@ -28,9 +28,35 @@ def correcao_monetaria_ipca(valor_passado, ipca, data_passada, data_correcao):
 
 
 def formata_uf_regiao_sus(df):
+  
+  
+  
   regions = {"1":"Norte","2":'Nordeste',"3":'Sudeste',"4":'Sul',"5":'Centro-oeste'}
   df = df.rename(columns={"Unidade da Federação": "UF"})
   df.insert(0, 'regiao',  df.index.str.slice(0,1) ) 
   df.index = df.index.str.slice(3,)
   df = df.replace({"regiao" : regions})
+  
+  
   return df
+
+def para_data(ano_mes: str):
+  meses = {
+  "Jan" = 1,
+  "Fev" = 2,
+  "Mar" = 3,
+  "Abr" = 4,
+  "Mai" = 5,
+  "Jun" = 6,
+  "Jul" = 7,
+  "Ago" = 8,
+  "Set" = 9,
+  "Out" = 10,
+  "Nov" = 11,
+  "Dez" = 12,
+  } 
+  ano: int = int(ano_mes[:4])
+  mes: str = ano_mes[5:]
+  mes_numero: int = meses[mes]
+    
+  return date(ano, mes_numero, 1)
